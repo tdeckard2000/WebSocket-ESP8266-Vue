c3db339808420356974efa86fa999b290e4a33f5
Trent Deckard <tredeckard@gmail.com>
2024-10-13 23:14:35 -0600

    Modify build script

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { WebSocketServer } from 'ws'
 import { createServer } from 'http'
-import process from 'process'
 
 const app = express()
 const __filename = fileURLToPath(import.meta.url)
